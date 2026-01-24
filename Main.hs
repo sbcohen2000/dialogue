71670920d07a61fb26c878c4745cd6e573b49e84
Sam Cohen <sbcohen2000@gmail.com>
2026-01-24 11:02:35 -0600

    Change the name to something more interesting

--- Main.hs
+++ Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import Options.Applicative
+
+data Options
+  = Options
+    {
+    }
+
+optionsParser :: Parser Options
+optionsParser = pure Options
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+  where
+    opts = info (optionsParser <**> helper)
+      ( fullDesc
+      <> progDesc "Test TARGET executable against the provided stdin/out dialogues." )
